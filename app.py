39062846e132f514dccaaa2bfd0e4d4038c97f52
Denis Ulvanskyi <denis.ulvanskyi@cengage.com>
2023-09-09 11:07:31 +0300

    fixed CORS, added origins

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from qr_code_generator import QRcode
 
 # uvicorn app:app --reload
 
 app = FastAPI(title="Test API")
 qr = QRcode()
+
+
+# Temporary solution for CORS policy
+origins = [
+    "http://localhost:5173",
+    "https://ui-qr-git-develop-denysmoon.vercel.app",
+    "https://ui-gwjhgia98-denysmoon.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
